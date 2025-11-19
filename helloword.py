805bbd8566eb83f9db68d0ceed4fe4f90ada4bdc
Kauã Nascimento <nascimento24122007@gmail.com>
2025-11-19 11:47:40 -0300

    Add files via upload

---
+++ helloword.py
@@ -0,0 +1,4 @@
+nome = input('Digite seu nome:{nome} ')
+sobrenome = input('Digite seu sobrenome: {sobrenome} ')
+idade = input('digite sua idade:{idade} ')
+print(f'Hello {nome}, com o sobremenome {sobrenome}, sua idade é de {idade}')
