d92bee0179a02c784fe18d5593ab0d37cd570847
Kauã Nascimento <nascimento24122007@gmail.com>
2025-11-24 11:57:02 -0300

    Add files via upload

---
+++ IF_ELIF_ELSE/EXEMPLOS PYTHON/jogo.py
@@ -0,0 +1,9 @@
+import os
+
+nome_do_jogo = input('Descreva o nome do jogo: ')
+valor__do_jogo = float(input('Me informe o valor do jogo: '))
+qtd_de_gb = float(input('Quantos GB ele consome do console? '))
+
+print('Jogo:', nome_do_jogo)
+print('valor:',valor__do_jogo)
+print('GB consumido:', qtd_de_gb)
