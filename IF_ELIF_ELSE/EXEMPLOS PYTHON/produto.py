05140ee3923cd80ea8218917a06f225c5e5ba0f6
Kauã Nascimento <nascimento24122007@gmail.com>
2025-11-24 11:57:28 -0300

    Delete IF_ELIF_ELSE/EXEMPLOS PYTHON directory

--- IF_ELIF_ELSE/EXEMPLOS PYTHON/produto.py
+++
@@ -1,8 +0,0 @@
-import os
-os.system('cls')
-nome_produto =  input('Digite o nome do produto: ')
-preco = float(input('Digite o preço do produto: '))
-desconto = float(input('Digite o valor do desconto:(%) '))
-valor_desconto = preco *desconto / 100
-preco_final = preco - valor_desconto
-print(f'Produto: {nome_produto} - preço final: ${preco_final}')
