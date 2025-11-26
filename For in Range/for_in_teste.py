82b8dd8e8b97bcada6b25257bee1e15425fce32e
Kauã Nascimento <nascimento24122007@gmail.com>
2025-11-26 11:40:11 -0300

    Add files via upload

---
+++ For in Range/for_in_teste.py
@@ -0,0 +1,7 @@
+import os 
+os.system('cls')
+
+numero = 5 #variavel que simboliza o número da tabuada 
+
+for i in range(1,11): #declarando o limite da tabuada ( o 'i' siginica o valor do mutiplicador)
+    print(f'{i} * {numero} = {i * numero}')  #calculando a variavel
